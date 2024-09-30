52e23e9234f5163bc6857c6c510c6068dbdea3e2
Velnbur <kyrylo.baybula@distributedlab.com>
2024-09-30 21:04:21 +0300

    :construction: :test_tube: add test scripts that will be used for verification

--- splitter/src/split/script.rs
+++ splitter/src/split/script.rs
@@ -1,72 +1,47 @@
 //! Module containing the structure for scripts that we are going to use
 
-use super::{core::naive_split, intermediate_state::IntermediateState};
+use bitcoin_script_stack::debugger::pushable::Pushable;
+
 use crate::treepp::*;
 
 /// Structure that represents a pair of input and output scripts. Typically, the prover
 /// wants to prove `script(input) == output`
-pub struct IOPair<const INPUT_SIZE: usize, const OUTPUT_SIZE: usize> {
-    /// Input script containing the elements which will be fed to the main script
-    pub input: Script,
-    /// Output script containing the elements which will be compared to the output of the main script
-    pub output: Script,
-}
-
-/// Structure that represents the result of splitting a script
-pub struct SplitResult {
-    /// Scripts (shards) that constitute the input script
-    pub shards: Vec<Script>,
-    /// Scripts that contain intermediate states (z values in the paper)
-    pub intermediate_states: Vec<IntermediateState>,
-}
-
-impl SplitResult {
-    /// Creates a new instance of the SplitResult
-    pub fn new(shards: Vec<Script>, intermediate_states: Vec<IntermediateState>) -> Self {
-        Self {
-            shards,
-            intermediate_states,
-        }
-    }
-
-    /// Returns the last intermediate state, ignoring the possibility of the empty vector
-    pub fn must_last_state(&self) -> &IntermediateState {
-        self.intermediate_states
-            .last()
-            .expect("Intermediate states should not be empty")
-    }
-
-    /// Returns the total size of the states (stack + altstack)
-    pub fn total_states_size(&self) -> usize {
-        self.intermediate_states
-            .iter()
-            .map(|state| state.stack.len() + state.altstack.len())
-            .sum()
-    }
+pub struct IOPair<
+    I, O: Pushable,
+    const INPUT_SIZE: usize, 
+    const OUTPUT_SIZE: usize,
+> {
+    pub input: [I; INPUT_SIZE],
+    pub output: [O; OUTPUT_SIZE],
 }
 
 /// Trait that any script that can be split should implement
-pub trait SplitableScript<const INPUT_SIZE: usize, const OUTPUT_SIZE: usize> {
-    const INPUT_SIZE: usize = INPUT_SIZE;
-    const OUTPUT_SIZE: usize = OUTPUT_SIZE;
-
+pub trait SplitableScript<I, O: Pushable, const INPUT_SIZE: usize, const OUTPUT_SIZE: usize> {
     /// Returns the main logic (f) of the script
     fn script() -> Script;
 
     /// Generates a random valid input for the script
-    fn generate_valid_io_pair() -> IOPair<INPUT_SIZE, OUTPUT_SIZE>;
+    fn generate_valid_io_pair() -> IOPair<I, O, INPUT_SIZE, OUTPUT_SIZE>;
 
     /// Verifies that the input is valid for the script
-    fn verify(input: Script, output: Script) -> bool {
+    fn verify(input: [I; INPUT_SIZE], output: [O; OUTPUT_SIZE]) -> bool {
+        let output_length = output.len();
+        println!("Output length: {}", output_length);
+
         let script = script! {
-            { input }
+            for i in 0..input.len() {
+                { input[i] }
+            }
+
             { Self::script() }
-            { output }
+            for i in 0..input.len() {
+                { output[i] }
+            }
 
             // Now, we need to verify that the output is correct.
             // Since the output is not necessarily a single element, we check
             // elements one by one
-            for i in (0..OUTPUT_SIZE).rev() {
+            for i in (0..output_length).rev() {
                 // { <a_1> <a_2> ... <a_n> <b_1> <b_2> ... <b_n> } <- we need to push element <a_n> to the top of the stack
                 { i+1 } OP_ROLL
                 OP_EQUALVERIFY
@@ -83,10 +58,5 @@ pub trait SplitableScript<const INPUT_SIZE: usize, const OUTPUT_SIZE: usize> {
     fn verify_random() -> bool {
         let IOPair { input, output } = Self::generate_valid_io_pair();
         Self::verify(input, output)
-    }
-
-    /// Splits the script into smaller parts
-    fn split(input: Script) -> SplitResult {
-        naive_split(input, Self::script())
     }
 }
--- splitter/src/test_scripts/int_add.rs
+++ splitter/src/test_scripts/int_add.rs
@@ -1,17 +1,13 @@
 //! This module contains the test script
-//! for performing the addition of two large integers
-//! (exceeding standard Bitcoin 31-bit integers)
+//! for performing the addition of two large integers (exceeding standard Bitcoin 31-bit integers)
 
 use crate::{
     split::script::{IOPair, SplitableScript},
     treepp::*,
 };
 use bitcoin_window_mul::{
-    bigint::U254,
-    traits::{
-        arithmeticable::Arithmeticable,
-        integer::{NonNativeInteger, NonNativeLimbInteger},
-    },
+    bigint::{U254Windowed, U254},
+    traits::{arithmeticable::Arithmeticable, integer::NonNativeInteger},
 };
 
 use core::ops::{Rem, Shl};
@@ -23,107 +19,38 @@ use rand_chacha::ChaCha20Rng;
 /// Script that performs the addition of two 254-bit numbers
 pub struct U254AddScript;
 
-/// Input size is double the number of limbs of U254 since we are adding two numbers
-const INPUT_SIZE: usize = 2 * U254::N_LIMBS;
-/// Output size is the number of limbs of U254
-const OUTPUT_SIZE: usize = U254::N_LIMBS;
-
-impl SplitableScript<{ INPUT_SIZE }, { OUTPUT_SIZE }> for U254AddScript {
+impl SplitableScript for U254AddScript {
     fn script() -> Script {
         U254::OP_ADD(1, 0)
     }
 
-    fn generate_valid_io_pair() -> IOPair<{ INPUT_SIZE }, { OUTPUT_SIZE }> {
+    fn generate_valid_io_pair() -> IOPair {
         let mut prng = ChaCha20Rng::seed_from_u64(0);
 
-        // Generate two random 254-bit numbers and calculate their sum
-        let num_1: BigUint = prng.sample(RandomBits::new(254));
-        let num_2: BigUint = prng.sample(RandomBits::new(254));
-        let sum: BigUint = (num_1.clone() + num_2.clone()).rem(BigUint::one().shl(254));
+        let a: BigUint = prng.sample(RandomBits::new(254));
+        let b: BigUint = prng.sample(RandomBits::new(254));
+        let c: BigUint = (a.clone() + b.clone()).rem(BigUint::one().shl(254));
+
+        println!("a: {}", a);
+        println!("b: {}", b);
+        println!("c: {}", c);
 
         IOPair {
             input: script! {
-                { U254::OP_PUSH_U32LESLICE(&num_1.to_u32_digits()) }
-                { U254::OP_PUSH_U32LESLICE(&num_2.to_u32_digits()) }
+                { U254::OP_PUSH_U32LESLICE(&a.to_u32_digits()) }
+                { U254::OP_PUSH_U32LESLICE(&b.to_u32_digits()) }
             },
-            output: U254::OP_PUSH_U32LESLICE(&sum.to_u32_digits()),
+            output: U254::OP_PUSH_U32LESLICE(&c.to_u32_digits()),
         }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use bitcoin_window_mul::traits::comparable::Comparable;
-
-    use crate::utils::stack_to_script;
-
     use super::*;
 
     #[test]
     fn test_verify() {
         assert!(U254AddScript::verify_random());
-    }
-
-    #[test]
-    fn test_naive_split_correctness() {
-        // Generating a random valid input for the script and the script itself
-        let IOPair { input, output } = U254AddScript::generate_valid_io_pair();
-        assert!(
-            U254AddScript::verify(input.clone(), output.clone()),
-            "input/output is not correct"
-        );
-
-        // Splitting the script into shards
-        let split_result = U254AddScript::split(input.clone());
-
-        // Now, we are going to concatenate all the shards and verify that the script is also correct
-        let verification_script = script! {
-            { input }
-            for shard in split_result.shards {
-                { shard }
-            }
-            { output }
-
-            // Now, we need to verify that the output is correct.
-            for i in (0..U254AddScript::OUTPUT_SIZE).rev() {
-                // { <a_1> <a_2> ... <a_n> <b_1> <b_2> ... <b_n> } <- we need to push element <a_n> to the top of the stack
-                { i+1 } OP_ROLL
-                OP_EQUALVERIFY
-            }
-
-            OP_TRUE
-        };
-
-        let result = execute_script(verification_script);
-        assert!(result.success, "Verification has failed");
-    }
-
-    #[test]
-    fn test_naive_split() {
-        // First, we generate the pair of input and output scripts
-        let IOPair { input, output } = U254AddScript::generate_valid_io_pair();
-
-        // Splitting the script into shards
-        let split_result = U254AddScript::split(input);
-
-        // Checking the last state (which must be equal to the result of the multiplication)
-        let last_state = split_result.must_last_state();
-
-        // Altstack must be empty
-        assert!(last_state.altstack.is_empty(), "altstack is not empty!");
-
-        // The element of the mainstack must be equal to the actual output
-        let verification_script = script! {
-            { stack_to_script(&last_state.stack) }
-            { output }
-            { U254::OP_EQUAL(0, 1) }
-        };
-
-        let result = execute_script(verification_script);
-        assert!(result.success, "verification has failed");
-
-        // Now, we debug the total size of the states
-        let total_size = split_result.total_states_size();
-        println!("Total size of the states: {} bytes", total_size);
     }
 }
