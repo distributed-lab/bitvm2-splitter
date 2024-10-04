700a4a78a6fc48bdaa91ec40f5045acb4aa0c301
ZamDimon <zamdmytro@gmail.com>
2024-10-04 11:18:33 +0300

    :interrobang: preparing for merge

--- splitter/src/split/mod.rs
+++
@@ -1,9 +0,0 @@
-//! Module that contains the implementation of the splitter
-//! together with all auxiliary functions and data structures.
-
-pub mod core;
-pub mod intermediate_state;
-pub mod script;
-
-#[cfg(test)]
-pub mod tests;
